4d6dcdd79a7820904fb0a8081bac5963aab46002
alvin2396 <alvin2396@yahoo.com>
2018-06-20 23:34:45 +0700

    first commit

---
+++ gamestudio/api/models/Games.js
@@ -0,0 +1,100 @@
+/**
+ * Games.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    game_id : {
+        type: 'string'
+    },
+
+    game_name : {
+        type : 'string'
+    },
+
+    release_date : {
+        type : 'string'
+    },
+
+    genre:{
+        type: 'string'
+    },
+
+    screenshot_url : {
+        type: 'string'
+    },
+
+    photo_url : {
+        type : 'string'
+    },
+
+    video_url : {
+        type : 'string'
+    },
+
+    harga : {
+        type : 'number'
+    },
+
+    min_requirement : {
+        model : 'spesifikasi'
+    },
+
+    recommended_requirement : {
+        model : 'spesifikasi'
+    },
+
+    discount : {
+        type : 'number'
+    },
+
+    description : {
+        type : 'string'
+    },
+
+    OS : {
+        type : 'string'
+    },
+
+    HDD_space : {
+        type : 'string'
+    },
+
+    rating : {
+        type : 'number'
+    },
+
+    Story_rating : {
+        type : 'number'
+    },
+
+    gameplay_rating : {
+        type : 'number'
+    },
+
+    controls_rating : {
+        type : 'number'
+    },
+
+    music_rating : {
+        type : 'number'
+    },
+
+    graphic_rating : {
+        type : 'number'
+    },
+
+    publisher : {
+        type : 'string'
+    },
+
+  },
+  connection : 'database'
+
+};
+
