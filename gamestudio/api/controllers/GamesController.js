83e5b43d68b7bb23d9cfa99476fbb184cafc1e82
alvin2396 <alvin2396@yahoo.com>
2018-06-28 00:40:02 +0700

    login register session

--- gamestudio/api/controllers/GamesController.js
+++ gamestudio/api/controllers/GamesController.js
@@ -6,9 +6,67 @@
  */
 
 module.exports = {
-  add:function(req,res){
-  		res.view('admin/addGame')
-  }
+  detailGame: function (req, res, next) {
+       
+        Games.findOne(req.param('id')).populateAll().exec(function (err, game) {
+            if (err) {
+                return res.serverError(err);
+            } else {
+                
+                games.genreStrings = []
+                games.userStrings =[]
+                async.each(games.genre_lists, function (genre, callback) {
+                    Genre.findOne({ id: genre.id_genre }).exec(function (err, genres) {
+                        if (err) {
+                            callback(err)
+                        } else {
+                            
+                            games.genreStrings.push({
+                                id:genres.id,
+                                nama_genre:genres.nama_genre})
+                            callback()
+                        }
+                    })
+                }, function (err) { // function ini akan jalan bila semua genre_lists telah diproses
+                
+                    if (err)
+                        return res.serverError(err);
+                    else { 
+                        async.each(games.ratings, function (user, callback) {
+                            
+                            User.findOne({ id: user.id_user }).exec(function (err, users) {
+                                if (err) {
+                                    callback(err)
+                                } else {
+                                        games.userStrings.push({
+                                        id:users.id,
+                                        nama:users.nama,
+                                        photo_url:users.photo_url,users,
+                                        review:users.review,
+                                        score:user.score,
+                                        
+                                    })
+                                    callback()
+                                }
+                            })
+                        }, function (err) { // function ini akan jalan bila semua genre_lists telah diproses
+                           
+                            if (err)
+                                return res.serverError(err);
+                            else {         
+                                res.view("user/detail-games/", {
+                                    status: 'OK',
+                                    title: 'Detail games',
+                                    games: games
+                                })
+                            }
+                        })        
+                        
+                    }
+                })
 
+            }
+        })
+    }
 };
 
--- gamestudio/api/models/User.js
+++ gamestudio/api/models/User.js
@@ -4,22 +4,30 @@
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
-
+var bcrypt = require('bcrypt')
 module.exports = {
 
   attributes: {
 
     username:{
           type: 'string',
-          required:true
       },
       email:{
           type: 'string',
-          required:true
+          required:true,
+          unique:true
       },
       password:{
           type: 'string',
           required:true
+      },
+
+      nama:{
+        type: 'string',
+      },
+
+      alamat:{
+        type:'string',
       },
       
       toJSON: function(){
@@ -27,10 +35,35 @@ module.exports = {
       delete obj.password;
       delete obj.csrf;
       return obj;
-
-    
     },
+  },
 
+  beforeCreate: function(user, cb) {
+      bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) {
+                console.log(err);
+                cb(err);
+            } else {
+                user.password = hash;
+                console.log(hash);
+                cb(null, user);
+            }
+          });
+      });
+  },
+  
+  
+  comparePassword : function (password, user, cb) {
+    bcrypt.compare(password, user.password, function (err, match) {
+
+      if(err) cb(err);
+      if(match) {
+        cb(null, true);
+      } else {
+        cb(err);
+      }
+    });
   },
   connection:'database'
 };
